a858a3d0d74836f4ce8596948335fb113e31c188
DeanPham <dean.pham@boostcommerce.net>
2023-07-10 21:20:29 +0700

    update

--- index.php
+++ index.php
@@ -26,18 +26,32 @@
 			</div>
 		</div>
 
+		<div class="p-about p-section">
+			<div class="container">
+				<div class="p-about__wrap">
+					<div class="p-about__video">
+						<iframe src="https://www.youtube.com/embed/IHNzOHi8sJs" title="BLACKPINK - ‘뚜두뚜두 (DDU-DU DDU-DU)’ M/V" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+					</div>
+					<div class="p-about__content">
+						<h2 class="hline02 font-500">Vườn Của By</h2>
+						<p class="p-about__txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet odit nostrum qui modi ipsum eum dignissimos sunt quibusdam et inventore saepe quasi quidem libero recusandae sapiente, tempora nesciunt dolores ab!<br><br>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam quaerat mollitia harum, ducimus quia quisquam aliquid dolore provident recusandae vel libero repellendus, similique ipsum debitis dicta omnis, laborum totam inventore.<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse soluta rerum animi minus quaerat possimus, reiciendis provident amet tempore voluptatibus accusamus molestiae cum, ullam id, quia ad debitis adipisci voluptatum.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<div class="p-prd">
 			<div class="p-prd__wrap">
 				<div class="container">
 					<div class="p-prd__col">
-						<h2 class="hline02">THỰC ĐƠN CỦA CHÚNG TÔI</h2>
+						<h2 class="hline03 font-500">THỰC ĐƠN CỦA CHÚNG TÔI</h2>
 						<ul class="lst-card">
 							<li class="lst-card__item">
 								<div class="lst-card__media">
 									<a href="#"><img src="assets/images/products/img_3873-1_ee2bcc7639324c8a94a7c11d4d6b698a_grande.webp" class="full-image" alt="Ốc Hương Pháp"></a>
 								</div>
 								<div class="lst-card__content">
-									<h2 class="lst-card__ttl"><a href="#">Ốc Hương Pháp</a></h2>
+									<h2 class="lst-card__ttl"><a href="#">ỐC HƯƠNG PHÁP 100G</a></h2>
 									<p class="txt-note">Tối thiếu 300g</p>
 								</div>
 								<p class="lst-card__price">81.000đ</p>
@@ -47,7 +61,7 @@
 									<a href="#"><img src="assets/images/products/img_3873-1_ee2bcc7639324c8a94a7c11d4d6b698a_grande.webp" class="full-image" alt="Ốc Hương Pháp"></a>
 								</div>
 								<div class="lst-card__content">
-									<h2 class="lst-card__ttl"><a href="#">Ốc Hương Pháp</a></h2>
+									<h2 class="lst-card__ttl"><a href="#">NGAO 2 CÒI 100G (TỐI THIỂU 300G)</a></h2>
 									<p class="txt-note">Tối thiếu 300g</p>
 								</div>
 								<p class="lst-card__price">81.000đ</p>
@@ -57,7 +71,7 @@
 									<a href="#"><img src="assets/images/products/img_3873-1_ee2bcc7639324c8a94a7c11d4d6b698a_grande.webp" class="full-image" alt="Ốc Hương Pháp"></a>
 								</div>
 								<div class="lst-card__content">
-									<h2 class="lst-card__ttl"><a href="#">Ốc Hương Pháp</a></h2>
+									<h2 class="lst-card__ttl"><a href="#">CÁ MÚ</a></h2>
 									<p class="txt-note">Tối thiếu 300g</p>
 								</div>
 								<p class="lst-card__price">81.000đ</p>
@@ -67,32 +81,43 @@
 									<a href="#"><img src="assets/images/products/img_3873-1_ee2bcc7639324c8a94a7c11d4d6b698a_grande.webp" class="full-image" alt="Ốc Hương Pháp"></a>
 								</div>
 								<div class="lst-card__content">
-									<h2 class="lst-card__ttl"><a href="#">Ốc Hương Pháp</a></h2>
+									<h2 class="lst-card__ttl"><a href="#">CÁ CHẼM - 100G</a></h2>
 									<p class="txt-note">Tối thiếu 300g</p>
 								</div>
 								<p class="lst-card__price">81.000đ</p>
 							</li>
 						</ul>
+						<p><a href="tel:+0979166661" class="btn btn-order mt-30">Xem tất cả</a></p>
 					</div>
 					<div class="p-prd__col">
-						<h2 class="hline02">GỌI ĐẶT BÀN</h2>
-						<div>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos inventore quisquam possimus illum. Nemo eos vero quibusdam repellendus inventore sapiente commodi error aliquid, illo aliquam quo dolores eveniet laboriosam veritatis?<br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos inventore quisquam possimus illum. Nemo eos vero quibusdam repellendus inventore sapiente commodi error aliquid, illo aliquam quo dolores eveniet laboriosam veritatis?<br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos inventore quisquam possimus illum. Nemo eos vero quibusdam repellendus inventore sapiente commodi error aliquid, illo aliquam quo dolores eveniet laboriosam veritatis?</p>
-							<div class="time-work">
-								<div class="time-work__box">
-									<div class="time-work__icon">
-										<img src="//theme.hstatic.net/200000542971/1000913900/14/img_home_main_order_right_2.png?v=416" alt="THỜI GIAN NHÀ HÀNG MỞ CỬA">
-									</div>
-									<div class="time-work__content">
-										<h4>THỜI GIAN NHÀ HÀNG MỞ CỬA</h4>
-										<p><strong>Thứ 2 đến Chủ Nhật<br>11:00 AM - 23:59 PM</strong></p>
-									</div>
+						<div class="time-work">
+							<h2 class="hline03 font-500 txt-white">GỌI ĐẶT BÀN</h2>
+							<p class="time-work__des">Chúng tôi có một loạt thực đơn về các món nướng BBQ sẵn sàng làm siêu lòng những gangsta sành ăn cùng menu đồ uống đa dạng để tăng sự trải nghiệm các cung bậc cảm xúc khác nhau.<br><br>Ngoài phục vụ các món nướng BBQ, chúng tôi còn có thực đơn dành cho những tín đồ đam mê món Nhật với set Sashimi hoành tráng chiều lòng thực khách.<br><br>Chắc chắn ngoài sự ngon miệng từ món ăn, sự thoải mái thông qua không gian và cách phục vụ, các nguyên liệu được chúng tôi sử dụng đều tươi mới và tốt cho sức khỏe của bạn. </p>
+							<div class="time-work__box">
+								<div class="time-work__icon">
+									<img src="//theme.hstatic.net/200000542971/1000913900/14/img_home_main_order_right_2.png?v=416" alt="THỜI GIAN NHÀ HÀNG MỞ CỬA">
 								</div>
-								<button class="btn btn-order">Gọi Đặt Bàn</button>
+								<div class="time-work__content">
+									<h4>THỜI GIAN NHÀ HÀNG MỞ CỬA</h4>
+									<p><strong>Thứ 2 đến Chủ Nhật<br>11:00 AM - 23:59 PM</strong></p>
+								</div>
 							</div>
+							<p><a href="tel:+0979166661" class="btn btn-order">Gọi đặt bàn</a></p>
 						</div>
 					</div>
 				</div>
+			</div>
+		</div>
+
+		<div class="p-try p-section">
+			<div class="container">
+				<h2 class="hline02 font-500">Những Món Ăn Nên Thử</h2>
+			</div>
+		</div>
+
+		<div class="p-news p-section">
+			<div class="container">
+				<h2 class="hline02 font-500">Tin Tức</h2>
 			</div>
 		</div>
 	</main>
--- products.php
+++ products.php
@@ -8,10 +8,7 @@
 	<meta name="description" content="{[#SITE_DESCRIPTION#]}">
 	<meta name="keywords" content="{[#SITE_KEYWORDS#]}">
 	<title>Vườn Cũa By</title>
-	<!-- <?php include('inc/head.php'); ?> -->
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet">
+	<?php include('inc/head.php'); ?>
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -23,18 +20,47 @@
             <div class="p-mv__media" style="background-image: url(assets/images/about/banner.jpg);"></div>
             <h2 class="p-mv__ttl">Về Chúng Tôi</h2>
         </div>
-        <div class="p-about p-section">
-            <div class="container">
-                <h1 class="hline02 text-center mb-20">Giới thiệu Vườn Của By</h1>
-                <div class="p-content">
-                    <p><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?<br><br><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?<br><br><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?<br><br><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?</p><br><br>
-                    <p><img src="assets/images/about/watermelon-g86b5c79c2_1920.jpg" alt=""></p><br><br>
-                    <p><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?<br><br><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?<br><br><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?<br><br><strong>Tiêu Đề</strong><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit officiis maxime itaque eaque fuga accusantium debitis repudiandae id excepturi quam totam dignissimos vel consequatur quibusdam eligendi placeat est, incidunt aut?</p>
-                </div>
-            </div>
-        </div>
 
-        
+		<div class="p-products p-section">
+			<div class="container">
+				<div class="d-flex">
+					<div class="sidebar">
+						<ul>
+							<li><a href="#">Lai Rai</a></li>
+							<li><a href="#">Lai Rai</a></li>
+							<li><a href="#">Lai Rai</a></li>
+							<li><a href="#">Lai Rai</a></li>
+							<li><a href="#">Lai Rai</a></li>
+							<li><a href="#">Lai Rai</a></li>
+							<li><a href="#">Lai Rai</a></li>
+						</ul>
+					</div>
+					<div class="lst-prd">
+						<div class="lst-prd__item">
+							<a href="#" class="lst-prd__media">
+								<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt="">
+							</a>
+							<h2 class="lst-prd__ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+							<p class="lst-prd__price">264,600₫</p>
+						</div>
+						<div class="lst-prd__item">
+							<a href="#" class="lst-prd__media">
+								<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt="">
+							</a>
+							<h2 class="lst-prd__ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+							<p class="lst-prd__price">264,600₫</p>
+						</div>
+						<div class="lst-prd__item">
+							<a href="#" class="lst-prd__media">
+								<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt="">
+							</a>
+							<h2 class="lst-prd__ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+							<p class="lst-prd__price">264,600₫</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main>
 
 	<?php include('inc/footer.php'); ?>
