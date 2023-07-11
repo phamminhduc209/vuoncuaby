f63cc9eb13d32611ef2a8962d5848caef976b170
DeanPham <dean.pham@boostcommerce.net>
2023-07-11 21:22:56 +0700

    update

---
+++ detail.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="description" content="{[#SITE_DESCRIPTION#]}">
+	<meta name="keywords" content="{[#SITE_KEYWORDS#]}">
+	<title>Vườn Cũa By</title>
+	<?php include('inc/head.php'); ?>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	
+	<?php include('inc/header.php'); ?>
+
+	<main>
+		<div class="p-mv wow">
+            <div class="p-mv__media" style="background-image: url(assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg);"></div>
+            <h2 class="p-mv__ttl">Menu Của Chúng Tôi</h2>
+        </div>
+
+		<div class="p-section">
+			<div class="p-detail">
+				<div class="container">
+					<div class="p-detail__row">
+						<div class="p-detail__image">
+							<div class="swiper mySwiper2">
+								<div class="swiper-wrapper">
+									<div class="swiper-slide">
+										<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344368046_730746248800884_4632771107745808032_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344407132_169075279121413_4188415114297232395_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344368046_730746248800884_4632771107745808032_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344407132_169075279121413_4188415114297232395_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+									</div>
+								</div>
+							</div>
+							<div thumbsSlider="" class="swiper mySwiper p-mySwiper">
+								<div class="swiper-wrapper">
+									<div class="swiper-slide">
+										<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344368046_730746248800884_4632771107745808032_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344407132_169075279121413_4188415114297232395_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344368046_730746248800884_4632771107745808032_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344407132_169075279121413_4188415114297232395_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+										<img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" />
+									</div>
+									<div class="swiper-slide">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="p-detail__item">
+							<h1 class="hline03 mb-10" style="padding: 0 0 10px; border-bottom: 1px dotted #dfe0e1;">Chips lắc phô mai</h1>
+							<p class="txt-price">81,000₫</p>
+							<div class="product-description mt-40">
+								<div class="title-bl">
+									<p><strong>Bao gồm</strong></p>
+								</div>							
+								<div class="description-content">
+									<div class="description-productdetail">
+										<p>- Gà ta nguyên con</p>
+										<p>- Dưa leo</p>
+										<p>- Rau thơm các loại</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="p-relative mt-100">
+				<div class="container">
+					<h2 class="hline02">Món ăn khác</h2>
+					<div class="lst-prd">
+						<div class="lst-prd__item">
+							<a href="#">
+								<div class="media">
+									<span class="default"><img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt=""></span>
+									<span class="hover"><img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" alt=""></span>
+								</div>
+								<h2 class="ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+								<p class="prc">264,600₫</p>
+							</a>
+						</div>
+						<div class="lst-prd__item">
+							<a href="#">
+								<div class="media">
+									<span class="default"><img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt=""></span>
+									<span class="hover"><img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" alt=""></span>
+								</div>
+								<h2 class="ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+								<p class="prc">264,600₫</p>
+							</a>
+						</div>
+						<div class="lst-prd__item">
+							<a href="#">
+								<div class="media">
+									<span class="default"><img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt=""></span>
+									<span class="hover"><img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" alt=""></span>
+								</div>
+								<h2 class="ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+								<p class="prc">264,600₫</p>
+							</a>
+						</div>
+						<div class="lst-prd__item">
+							<a href="#">
+								<div class="media">
+									<span class="default"><img src="assets/images/products/344341043_1987049251634621_9004515427612487697_n.jpg" alt=""></span>
+									<span class="hover"><img src="assets/images/products/344410104_628554748734225_7257001745854457973_n.jpg" alt=""></span>
+								</div>
+								<h2 class="ttl"><a href="#" alt="Bò nướng sốt đậu phộng">Bò nướng sốt đậu phộng</a></h2>
+								<p class="prc">264,600₫</p>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php include('inc/footer.php'); ?>
+	
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/slick.min.js"></script>
+	<script src="assets/js/script.js"></script>
+
+	<script>
+		var swiper = new Swiper(".mySwiper", {
+			spaceBetween: 10,
+			slidesPerView: 6,
+			freeMode: true,
+			watchSlidesProgress: true,
+		});
+		var swiper2 = new Swiper(".mySwiper2", {
+			spaceBetween: 10,
+			navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+			},
+			thumbs: {
+			swiper: swiper,
+			},
+		});
+	</script>
+</body>
+</html>
--- inc/header.php
+++ inc/header.php
@@ -4,11 +4,11 @@
 <header class="p-header">
 	<div class="p-header__wrap">
 		<div class="p-header__logo">
-			<a href="/"><img src="assets/images/logo.png" alt=""></a>
+			<a href="index.php"><img src="assets/images/logo.png" alt=""></a>
 		</div>
 		<div class="p-header__menu">
 			<ul class="menu">
-				<li class="menu-item"><a href="#" class="is-active">Trang chủ</a></li>
+				<li class="menu-item"><a href="index.php" class="is-active">Trang chủ</a></li>
 				<li class="menu-item"><a href="products.php">Menu</a></li>
 				<li class="menu-item"><a href="about.php">Giới thiệu</a></li>
 				<li class="menu-item"><a href="news.php">Tin tức</a></li>
